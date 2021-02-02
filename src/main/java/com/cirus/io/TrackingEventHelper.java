282d8c6bc9fc492f223fed4ab08e5fad69f88cb5
Ardian Saidi <ardi@adaptivescale.com>
2021-02-02 14:12:18 +0100

    1. TrackingEvent with filter 2. TrackingEvent with filter and pagination

--- src/main/java/com/cirus/io/TrackingEventHelper.java
+++ src/main/java/com/cirus/io/TrackingEventHelper.java
@@ -5,7 +5,6 @@ import com.exacttarget.fuelsdk.ETFilter;
 import com.exacttarget.fuelsdk.ETResponse;
 import com.exacttarget.fuelsdk.ETResult;
 import com.exacttarget.fuelsdk.ETSdkException;
-import com.exacttarget.fuelsdk.ETSentEvent;
 import com.exacttarget.fuelsdk.ETSoapObject;
 
 import java.util.List;
@@ -16,11 +15,22 @@ public final class  TrackingEventHelper {
     try {
       System.out.println("Start " + type);
       ETResponse<T> retrieve = null;
-      retrieve = ETSentEvent.retrieve(client, type, null, null, filter);
+      retrieve = ETSoapObject.retrieve(client, type, null, null, filter);
       List<ETResult<T>> results = retrieve.getResults();
+//      console(results);
       System.out.println("Result " + retrieve.getResponseMessage() + " - " + type + " - " + results.size() );
     } catch (ETSdkException e) {
       e.printStackTrace();
     }
+  }
+
+  private static <T extends ETSoapObject> void console(List<ETResult<T>> rows) {
+    System.out.println("-- beatify --");
+    int counter=0;
+    for (ETResult<T> row : rows) {
+      System.out.print("Row ID " + counter++ + " ");
+      System.out.println(row.toString());
+    }
+    System.out.println("-- beatify --");
   }
 }
--- src/test/java/com/cirus/io/AppTest.java
+++ src/test/java/com/cirus/io/AppTest.java
@@ -9,6 +9,7 @@ import com.exacttarget.fuelsdk.ETFilter;
 import com.exacttarget.fuelsdk.ETOpenEvent;
 import com.exacttarget.fuelsdk.ETResponse;
 import com.exacttarget.fuelsdk.ETSdkException;
+import com.exacttarget.fuelsdk.ETSoapObject;
 import com.exacttarget.fuelsdk.ETUnsubEvent;
 import com.google.common.collect.Lists;
 import org.apache.log4j.BasicConfigurator;
@@ -17,6 +18,7 @@ import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,54 +28,60 @@ public class AppTest {
   private static ETClient client = null;
   private static String id = "";
   private static int debugIndex;
+  private static ArrayList<Class<? extends ETSoapObject>> eventTypes = Lists.newArrayList(ETOpenEvent.class,
+                                                                                          ETBounceEvent.class,
+                                                                                          ETClickEvent.class,
+                                                                                          ETUnsubEvent.class);
+
 
   @Before
   public void setup() throws ETSdkException {
     BasicConfigurator.configure();
     Logger.getRootLogger().setLevel(Level.ALL);
-    client = new ETClient("");
+
+    client = new ETClient(    System.getenv("fuelsdk_prop"));
   }
 
   @Test
   public void testDatExtractPagination() {
-      dataExtraction(new ETFilter());
+    dataExtraction(new ETFilter());
   }
 
   @Test
   public void testDatExtractFilterAndPagination() {
-      debugIndex = 0;
-      ETExpression etExpression = new ETExpression();
-      etExpression.setOperator(ETExpression.Operator.EQUALS);
-      etExpression.setProperty("pet_name");
-      etExpression.setValue("Reba");
+    debugIndex = 0;
+    ETExpression etExpression = new ETExpression();
+    etExpression.setOperator(ETExpression.Operator.EQUALS);
+    etExpression.setProperty("pet_name");
+    etExpression.setValue("Reba");
 
-      ETFilter etFilter = new ETFilter();
-      etFilter.setProperties(Lists.newArrayList("pet_gender",
-                                                "reproductive_status",
-                                                "source_of_pet",
-                                                "weight_lb",
-                                                "birth_dt",
-                                                "pet_name",
-                                                "favorite_brand",
-                                                "age",
-                                                "health_interests",
-                                                "alreadyexists",
-                                                "pet_create_dt",
-                                                "contact_id",
-                                                "pet_modify_dt",
-                                                "abilitec id",
-                                                "species",
-                                                "shelter_name",
-                                                "pet_id",
-                                                "breed_desc",
-                                                "breed_cd"
-      ));
-      etFilter.setExpression(etExpression);
-      dataExtraction(etFilter);
+    ETFilter etFilter = new ETFilter();
+    etFilter.setProperties(Lists.newArrayList("pet_gender",
+                                              "reproductive_status",
+                                              "source_of_pet",
+                                              "weight_lb",
+                                              "birth_dt",
+                                              "pet_name",
+                                              "favorite_brand",
+                                              "age",
+                                              "health_interests",
+                                              "alreadyexists",
+                                              "pet_create_dt",
+                                              "contact_id",
+                                              "pet_modify_dt",
+                                              "abilitec id",
+                                              "species",
+                                              "shelter_name",
+                                              "pet_id",
+                                              "breed_desc",
+                                              "breed_cd"
+    ));
+    etFilter.setExpression(etExpression);
+    dataExtraction(etFilter);
   }
 
   private static void dataExtraction(ETFilter etFilter) {
-      long startTime = System.currentTimeMillis();
+    long startTime = System.currentTimeMillis();
     try {
       ETResponse<ETDataExtensionRow> response = CustomETDataExtensionRow.select(client, "key=" + id, etFilter);
       System.out.println(response.getRequestId() + " " + response.getResponseCode() + " " + response.getResponseMessage());
@@ -100,6 +108,101 @@ public class AppTest {
     }
   }
 
+
+
+  @Test
+  public void testTrackingEvent() {
+    allTrackingEvents(new ETFilter());
+  }
+
+  @Test
+  public void testOpenTrackingEventWithFilter() throws ETSdkException {
+    ETFilter etFilter = ETFilter.parse("eventDate > 2010-01-01");
+    allTrackingEvents(etFilter);
+  }
+
+
+  /**
+   *  Simulate continue request id for open events
+   *  Currently in staging data set there are not enough data to test this scenario - max is 953
+   *  Simulate pagination by requesting 10 entities for page
+   *
+   * @throws ETSdkException
+   */
+  @Test
+  public void testBatchSizeForOpenEvents() throws ETSdkException {
+
+    long startTime = System.currentTimeMillis();
+    try {
+      ETResponse<ETOpenEvent> response = CustomETOpenEvent.customRetrieve(client, null, new ETFilter(), null, ETOpenEvent.class);
+      System.out.println(response.getRequestId() + " " + response.getResponseCode() + " " + response.getResponseMessage());
+      System.out.println(response.getTotalCount() + " " + response.getPageSize());
+      List<ETOpenEvent> rows = response.getObjects();
+      printEvents(rows);
+
+
+      while (response.getResponseMessage().equals("MoreDataAvailable")) {
+        System.out.println("----------------- Next -------------");
+        response = CustomETOpenEvent.customRetrieve(client, null, new ETFilter(), response.getRequestId(), ETOpenEvent.class);
+        System.out.println(response.getRequestId() + " " + response.getResponseCode() + " " + response.getResponseMessage());
+        System.out.println(response.getTotalCount() + " " + response.getPageSize());
+        printEvents(response.getObjects());
+      }
+
+      long endTime = System.currentTimeMillis();
+      System.out.println("Total Time " + (endTime - startTime) / 1000);
+
+    } catch (ETSdkException ex) {
+      System.out.println(ex);
+    } finally {
+      debugIndex = 0;
+    }
+  }
+
+  /**
+   *  Simulate continue request id for open events
+   *  Currently in staging data set there are not enough data to test this scenario - max is 953
+   *  Simulate pagination by requesting 10 entities for page
+   *
+   * @throws ETSdkException
+   */
+  @Test
+  public void testBatchSizeForOpenEventsWithFilter() throws ETSdkException {
+    ETFilter etFilter = ETFilter.parse("eventDate > 2020-12-01");
+    long startTime = System.currentTimeMillis();
+    try {
+      ETResponse<ETOpenEvent> response = CustomETOpenEvent.customRetrieve(client, null,etFilter, null, ETOpenEvent.class);
+      System.out.println(response.getRequestId() + " " + response.getResponseCode() + " " + response.getResponseMessage());
+      System.out.println(response.getTotalCount() + " " + response.getPageSize());
+      List<ETOpenEvent> rows = response.getObjects();
+      printEvents(rows);
+
+
+      while (response.getResponseMessage().equals("MoreDataAvailable")) {
+        System.out.println("----------------- Next -------------");
+        response = CustomETOpenEvent.customRetrieve(client, null, etFilter, response.getRequestId(), ETOpenEvent.class);
+        System.out.println(response.getRequestId() + " " + response.getResponseCode() + " " + response.getResponseMessage());
+        System.out.println(response.getTotalCount() + " " + response.getPageSize());
+        printEvents(response.getObjects());
+      }
+
+      long endTime = System.currentTimeMillis();
+      System.out.println("Total Time " + (endTime - startTime) / 1000);
+
+    } catch (ETSdkException ex) {
+      System.out.println(ex);
+    } finally {
+      debugIndex = 0;
+    }
+  }
+
+  private void allTrackingEvents(ETFilter filter) {
+    for (Class<? extends ETSoapObject > eventType : eventTypes) {
+      TrackingEventHelper.retrieveEvents(client, eventType, filter);
+    }
+  }
+
+
   private static void console(List<ETDataExtensionRow> rows) {
     for (ETDataExtensionRow row : rows) {
       System.out.print("Row ID " + debugIndex++ + " ");
@@ -110,21 +213,12 @@ public class AppTest {
     }
   }
 
-  @Test
-  public void testTrackingEvent() {
-      //todo unable to find event for
-      // 1. Not Sent
-      //    Send Jobs
-    //     Spam Complaints
-    //     Status Changes
-    //     SMS Message Tracking
-    //     SMS Subscription
-    //     Log and Undeliverable
-      TrackingEventHelper.retrieveEvents(client, ETBounceEvent.class, new ETFilter());
-      TrackingEventHelper.retrieveEvents(client, ETClickEvent.class, new ETFilter());
-      TrackingEventHelper.retrieveEvents(client, ETOpenEvent.class, new ETFilter());
-      TrackingEventHelper.retrieveEvents(client, ETUnsubEvent.class, new ETFilter());
+  private static void printEvents(List<ETOpenEvent> rows) {
+    for (ETOpenEvent row : rows) {
+      System.out.print("Row ID " + debugIndex++ + " ");
+      System.out.println(row.toString());
+      System.out.println();
+    }
   }
-
 
 }
